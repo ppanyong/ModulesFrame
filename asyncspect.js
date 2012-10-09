59cbf0ecdc355fcb343ef1ad893ee6031324e865
ppanyong <ppanyong@gmail.com>
2012-10-09 19:44:52 +0800

    部分变量重命名

--- asyncspect.js
+++ asyncspect.js
@@ -1,61 +1,60 @@
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     /**
      * VM Parser for Lazy Loading.
      * functions
      * 对暴露的方法做切面处理
-     * 需要将模块解析装配到模块积累中。是否需要一个页面级别的模块解析？
      */
     var $ = require('$');
     var Aspect = require('mi.util.Aspect');
     var cellula = require('cellula');
     var ajaxQueue = {
         queue:[],
-        call:function(config, callback, asyncspect) {
-            var tmpAjax = ajaxQueue.getAjaxByUrl(config.url);
+        call:function (module, callback, asyncspect) {
+            var tmpAjax = ajaxQueue.getAjaxQueueByUrl(module.url);
             if (tmpAjax) {
                 if (!$.isArray(tmpAjax.success)) {
                     tmpAjax.success = [tmpAjax.success];
                 }
-                tmpAjax.success.push(config[callback + '_Asyn']);
+                tmpAjax.success.push(module[callback + '_Asyn']);
             } else {
                 var tmpAjaxInstance = $.ajax({
-                    url: config.url + (config.refresh ? ('?t=' + new Date().getTime()) : ''),
-                    data: config.data,
-                    type: config.method,
-                    timeout: config.timeout,
-                    dataType: config.dataType,
-                    success: [function(response) {
-                        ajaxQueue.delAjaxByUrl(config.url);
-                        if (config.dataType == 'text') {
-                            $(config.rootNode).html($(response));
-                            (config._fill_times)?config._fill_times++:(config._fill_times=1);
-                            seajs.log(config.__cid__+' dom has been  fill,times='+config._fill_times)
+                    url:module.url + ('?t=' + new Date().getTime()),
+                    data:module.data,
+                    type:module.method,
+                    timeout:module.timeout,
+                    dataType:module.dataType,
+                    success:[function (response) {
+                        ajaxQueue.delAjaxQueueByUrl(module.url);
+                        if (module.dataType == 'text') {
+                            $(module.rootNode).html($(response));
+                            (module._fill_times) ? module._fill_times++ : (module._fill_times = 1);
+                            //seajs.log(module.__cid__+' dom has been  fill,times='+module._fill_times)
                         }
-                        asyncspect._execute(config, callback);
-                        config.trigger('DOMLOADED', config);
+                        asyncspect._execute(module, callback);
+                        module.trigger('DOMLOADED', module);
                     }]
                 });
-                tmpAjaxInstance.url = config.url;
+                tmpAjaxInstance.url = module.url;
                 ajaxQueue.queue.push(tmpAjaxInstance);
             }
         },
-        getAjaxByUrl:function(url) {
+        getAjaxQueueByUrl:function (url) {
             var result = null;
-            $.each(ajaxQueue.queue, function(index, value) {
+            $.each(ajaxQueue.queue, function (index, value) {
                 if (value.url == url) {
-                    result =  value;
+                    result = value;
                 }
             });
             return result;
         },
-        delAjaxByUrl:function(url) {
+        delAjaxQueueByUrl:function (url) {
             var result = null;
-            $.each(ajaxQueue.queue, function(index, value) {
+            $.each(ajaxQueue.queue, function (index, value) {
                 if (value) {
                     if (value.url == url) {
                         ajaxQueue.queue.splice(index, 1);
-                        seajs.log('remove ajaxQueue :'+value.url);
-                        result =  value;
+                        //seajs.log('remove ajaxQueue :' + value.url);
+                        result = value;
                     }
                 }
             });
@@ -63,44 +62,40 @@ define(function(require, exports, module) {
         }
     };
     var asyncspect = {
-        _init: function(module) {
+        _init:function (module) {
             var node = module.rootNode;
             var config = {
                 /** 是否已同步加载*/
-                sync: !!node.attr('data-sync'),
+                sync:!!node.attr('data-sync'),
 
                 /** 数据请求返回类型html, text, script, xml, json, jsonp */
-                    // TODO should separate success func for different mime types
-                dataType: node.attr('data-type') || 'text',
+                // TODO should separate success func for different mime types
+                dataType:node.attr('data-type') || 'text',
 
                 /** 数据请求方式 get, post */
-                method: node.attr('data-method') || 'get',
+                method:node.attr('data-method') || 'get',
 
                 /** 请求超时时间 */
-                timeout: parseInt(node.attr('data-timeout')) || 0,
+                timeout:parseInt(node.attr('data-timeout')) || 0,
 
                 /** 请求url */
-                url: node.attr('href') || node.attr('data-url') || false
+                url:node.attr('href') || node.attr('data-url') || false
 
             };
             cellula._util.mix(module, config);
         },
 
-        _execute: function(module, callback) {
+        _execute:function (module, callback) {
             callback && module[callback + '_Asyn'] && (module[callback + '_Asyn']());
             asyncspect._clean(module);
         },
 
-        _clean: function(config) {
+        _clean:function (config) {
             seajs.log('syncspect._clean:' + config.url);
             config.sync = true;
         },
-        /**
-         * ajax get data||text||html
-         * @param config
-         * todo:需要加callback队列
-         */
-        _load: function(config, callback) {
+
+        _load:function (config, callback) {
             if (!config.sync) {
                 ajaxQueue.call(config, callback, asyncspect);
             } else {
@@ -111,20 +106,20 @@ define(function(require, exports, module) {
          * 对api暴露出来的方法，增加切面
          * @param module
          */
-        aspect:function(module) {
+        aspect:function (module) {
             this._init(module);
             var api = module.getApiMap();
             Aspect(module);
             var that = this;
-            if (!module.sync) {
-                for (var n in api) {
-                    if (module[api[n]]) {
-                        module.before(api[n], function(method, context) {
-                            that._load(context, method);
-                        }, true);
-                    }
+
+            for (var n in api) {
+                if (module[api[n]]) {
+                    module.before(api[n], function (method, context) {
+                        that._load(context, method);
+                    }, true);
                 }
             }
+
         }
 
     };
